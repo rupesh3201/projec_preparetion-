97af10a6cfdd385c82df73d22dd91b8c5363bef5
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-03 22:50:03 +0530

    Calling Function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function hello()
 {
-    console.log('hi');
-    document.write("Hellow its me Named Function or Normal Function")
+    console.log('Learning Functions ');
+   
 }
 hello();//named function and Normal function
 function send_Email(name)
@@ -17,11 +17,11 @@ function send_Sms(name)
 
 function send_Greatings(name, send_communication)
 {
-    console.log(`Hello${name}`);
+    console.log(`Hello ${name}`);
     console.log(send_communication);
 
 }
-send_Greatings('Rupesh',send_Email);
-send_Greatings('Harish',send_Email);
-send_Greatings('Funtya',send_Email );
-send_Greatings('Akash',send_Sms);
+send_Greatings('Rupesh',send_Email());
+send_Greatings('Harish',send_Email());
+send_Greatings('Funtya',send_Email() );
+send_Greatings('Akash',send_Sms());
