50439467be4425d1455bf133edfa06f94f080697
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-03 23:00:19 +0530

    / Higher Oeder function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function send_Sms(name)
 
 }
 
-function send_Greatings(name, send_communication)
+function send_Greatings(name, send_communication) //Higher Order Funtion
 {
     console.log(`Hello ${name}`);
     console.log(send_communication);
