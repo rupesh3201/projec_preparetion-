0b0b2be526690f76a8592e70a684878750329e3b
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-03 23:06:54 +0530

    callback / Higherorder function

--- Ex.js
+++ Ex.js
@@ -1,5 +1,5 @@
-function showCurrentTime()
+function showCurrentTime()// Higherorder function
 {
     console.log(new Date());
 }
-setInterval(showCurrentTime,1000);
+setInterval(showCurrentTime,1000);//callback
