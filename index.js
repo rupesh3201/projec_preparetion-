c9015b4692507845cd0591afdf2ffc42559eea16
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-03 22:41:52 +0530

    Simple Greatings

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hello()
 {
     console.log('hi');
-    document.write("Hellow its me Named Function")
+    document.write("Hellow its me Named Function or Normal Function")
+}
 hello();//named function and Normal function
+
+
+function send_Greatings(name)
+{
+    console.log(`Hello${name}`);
+
+}
+send_Greatings('Rupesh');
