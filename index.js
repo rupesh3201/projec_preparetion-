1f0a13f90fc9ea267951b854a1263011a2368899
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-03 22:46:54 +0530

    sedn sms

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function send_Email(name)
     console.log(`Send email to  ${name}`);
 
 }
+function send_Sms(name)
+{
+    console.log(`Send SMS to  ${name}`);
+
+}
 
 function send_Greatings(name, send_communication)
 {
@@ -18,4 +23,4 @@ function send_Greatings(name, send_communication)
 send_Greatings('Rupesh',send_Email);
 send_Greatings('Harish',send_Email);
 send_Greatings('Funtya',send_Email );
-send_Greatings('Akash',send_Email);
+send_Greatings('Akash',send_Sms);
