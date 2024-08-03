18a0ef8781d72b9d058045883ad2511bf51d6112
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-03 22:58:52 +0530

    / CALL back function

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function send_Greatings(name, send_communication)
     send_communication(name);
 
 }
-send_Greatings('Rupesh',send_Email);
-send_Greatings('Harish',send_Email);
-send_Greatings('Funtya',send_Email );
-send_Greatings('Akash',send_Sms);
+send_Greatings('Rupesh',send_Email); // CALL back function
+send_Greatings('Harish',send_Email); // CALL back function
+send_Greatings('Funtya',send_Email );// CALL back function
+send_Greatings('Akash',send_Sms);// CALL back function
